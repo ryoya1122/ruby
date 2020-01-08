604f05a6c03f3438b3b4fc82136923afc71c9063
ryoya1122 <koba.jht@gmail.com>
2020-01-08 15:10:51 +0900

    [Finish] 3-8

--- vagrant/sample_app/config/initializers/application_controller_renderer.rb
+++ vagrant/sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '23b17d971d91f7acefc8c543f4763fcfa6d6acd4332768f0597b2695a54edca0b4460576d7f792e9cfb61071d95a4618d518f36d1e5c799623d5b611c59546ff'
