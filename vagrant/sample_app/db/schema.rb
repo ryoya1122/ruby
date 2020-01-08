a0d4c077da6f7564a787d24f7be0b247101aa85b
ryoya1122 <koba.jht@gmail.com>
2020-01-08 14:26:35 +0900

    [Finish] 3-5

--- vagrant/sample_app/db/schema.rb
+++ vagrant/sample_app/db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_08_050416) do
+ActiveRecord::Schema.define(version: 2020_01_08_050448) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
   end
 
 end
